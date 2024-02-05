4c66da0dd1d10640987a7f145ef69d76e588fb2e
kuni <kunidesuyo1234@gmail.com>
2024-02-05 22:03:12 +0900

    4-2

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
+Route::get('/test', [TestController::class, 'test'])
+->name('test');
 /*
 |--------------------------------------------------------------------------
 | Web Routes
