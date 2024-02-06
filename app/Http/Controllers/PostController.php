52fde1a58ff720f9c36959cbaad38095cc7b8043
kuni <kunidesuyo1234@gmail.com>
2024-02-07 08:59:01 +0900

    9-3

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,8 +13,6 @@ class PostController extends Controller
     }
 
     public function store(Request $request) {
-        //Gate::authorize('test');
-
         $validated = $request->validate([
             'title' => 'required|max:20',
             'body' => 'required|max:400',
@@ -23,11 +21,6 @@ class PostController extends Controller
         $validated['user_id'] = auth()->id();
 
         $post = Post::create($validated);
-
-        // $post = Post::create([
-        //     'title' => $request->title,
-        //     'body' => $request->body
-        // ]);
 
         $request->session()->flash('message', '保存しました');
 
@@ -41,5 +34,24 @@ class PostController extends Controller
 
     public function show(Post $post) {
         return view('post.show', compact('post'));
+    }
+
+    public function edit(Post $post) {
+        return view('post.edit', compact('post'));
+    }
+
+    public function update(Request $request, Post $post) {
+        $validated = $request->validate([
+            'title' => 'required|max:20',
+            'body' => 'required|max:400',
+        ]);
+
+        $validated['user_id'] = auth()->id();
+
+        $post->update($validated);
+
+        $request->session()->flash('message', '更新しました');
+
+        return back();
     }
 }
